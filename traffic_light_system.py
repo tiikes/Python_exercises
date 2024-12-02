51f1fa67a8db960f40476fc8e64e3a36059a1781
tiikes <116830806+tiikes@users.noreply.github.com>
2024-12-02 17:03:44 +0200

    Delete traffic_light_system.py

--- traffic_light_system.py
+++
@@ -1,9 +0,0 @@
-traffic_lights = "green" #set traffic light to green
-if traffic_lights == "green": #if light is green, print "go"
-    print("GO!")
-elif traffic_lights == "yellow": #if light is yellow, print "caution"
-    print("Caution")
-elif traffic_lights == "red": #if light is red, print "stop"
-    print("Stop")
-else:
-    print("Error")
